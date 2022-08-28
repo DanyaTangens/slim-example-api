2d37d16cad8de228e795e07eda01c624d5a5c22c
Daniil <d.iluhin@k-telecom.org>
2022-08-28 21:36:25 +0500

    Hello World

--- src/Operation/HelloWorld.php
+++ src/Operation/HelloWorld.php
@@ -2,7 +2,19 @@
 
 namespace App\Operation;
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 class HelloWorld
 {
+    use InjectJsonInResponseTrait;
 
+    public function __invoke(Request $request, Response $response): Response
+    {
+        $data = [
+            'Hello' => 'World'
+        ];
+
+        return $this->injectJson($response, $data);
+    }
 }
--- src/Operation/InjectJsonInResponseTrait.php
+++ src/Operation/InjectJsonInResponseTrait.php
@@ -2,7 +2,17 @@
 
 namespace App\Operation;
 
+use Psr\Http\Message\ResponseInterface;
+
 trait InjectJsonInResponseTrait
 {
+    private function injectJson(ResponseInterface $response, $data): ResponseInterface
+    {
+        $body = $response->getBody();
+        $body->write(json_encode($data, JSON_THROW_ON_ERROR));
 
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withBody($body);
+    }
 }
